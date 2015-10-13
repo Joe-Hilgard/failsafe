74157892f035fb214c750c0cf284e194fffcc6e3
Joe Hilgard <jhilgard@asc.upenn.edu>
2015-10-13 17:12:10 -0400

    Adding more scenarios and elaborating on them.

--- failsafe.R
+++ failsafe.R
@@ -4,9 +4,111 @@
 library(metafor)
 library(truncdist)
 
+# Pub bias: 5% of nonsig results are published ----
+## You can see that publication bias alone is not likely to account for distorted literature
+## "Nobody has 19 studies in a file-drawer for every positive study."
+k = 100
+d.true = 0
+null.pub.prob = .05
+
+# choose sample sizes
+n.cell = rtrunc(k, spec = "pois", a = 12, b = 200, lambda = 30)
+df = n.cell*2 - 2
+
+# generate all studies
+t = rt(k, df, ncp = sqrt(n.cell/2)*d.true) 
+#convert to effect size etc.
+d=2*t/sqrt(2*n.cell)
+d_v = ( (2*n.cell / (n.cell^2)) + d^2 / (2*df) ) * (2*n.cell / df)
+d_se = sqrt(d_v)
+p=2*pt(t, df=df, lower.tail = F)
+
+# apply statistical significance threshold
+pub = rbinom(k, size = 1, prob = null.pub.prob)
+pub[p < .05] = 1
+
+# meta-analysis
+meta.2 =  rma(yi = d[pub == 1], vi = d_v[pub == 1])
+meta.2
+funnel(meta.2)
+# proportion significant
+table(p[pub == 1] < .05)
+# fail-safe N
+fsn(yi = d[pub == 1], vi = d_v[pub == 1], type = "Rosenthal")
+fsn(yi = d[pub == 1], vi = d_v[pub == 1], type = "Orwin")
+fsn(yi = d[pub == 1], vi = d_v[pub == 1], type = "Rosenberg")
+
+
+# QRP: Collect three measures, report one ----
+k = 100
+d.true = 0
+null.pub.prob = .05
+
+# choose sample sizes
+n.cell = rtrunc(k, spec = "pois", a = 12, b = 200, lambda = 30)
+df = n.cell*2 - 2
+
+# prepare holster
+result = data.frame("d" = NULL, "d_v" = NULL, "d_se" = NULL, "p" = NULL)
+
+for (i in 1:k) {
+  n.iter = n.cell[i]
+  df.iter = df[i]
+  dat = data.frame("x" = rep(0:1, each=n.iter),
+                   "y1" = rnorm(n.iter*2) + d.true*rep(0:1, each=n.iter),
+                   "y2" = rnorm(n.iter*2) + d.true*rep(0:1, each=n.iter),
+                   "y3" = rnorm(n.iter*2) + d.true*rep(0:1, each=n.iter)
+                   )
+  mod1 = lm(y1 ~ x, dat)
+  mod2 = lm(y2 ~ x, dat)
+  mod3 = lm(y3 ~ x, dat)
+  p1 = summary(mod1)$coefficients[2,4] 
+  p2 = summary(mod2)$coefficients[2,4]
+  p3 = summary(mod3)$coefficients[2,4]
+  t1 = summary(mod1)$coefficients[2,3] 
+  t2 = summary(mod2)$coefficients[2,3]
+  t3 = summary(mod3)$coefficients[2,3]
+  if (p1 < .05) {
+    t = t1
+    p = p1 
+  } else if (p2 < .05) {
+    t = t2
+    p = p2
+  } else if (p3 < .05) {
+    t = t3
+    p = p3
+  } else {
+    t = t1
+    p = p1
+  }
+    out = data.frame("d" = 2*t/sqrt(2*n.iter))
+    out$d_v = ( (2*n.iter / (n.iter^2)) + out$d^2 / (2*df.iter) ) * (2*n.iter / df.iter)
+    out$d_se = sqrt(out$d_v)
+    out$p = p
+# append
+    result = rbind(result, out)
+}
+
+# apply statistical significance threshold
+result$pub = rbinom(k, size = 1, prob = null.pub.prob)
+result$pub[result$p < .05 & result$d > 0] = 1
+
+# meta-analysis
+meta.3dv =  rma(yi = d, vi = d_v, data = result, subset = pub == 1)
+meta.3dv
+funnel(meta.3dv)
+# proportion significant
+table(result$p < .05)
+table(result$pub, result$p < .05)
+# fail-safe N
+fsn(yi = d, vi = d_v, data = result, subset = pub == 1, type = "Rosenthal")
+fsn(yi = d, vi = d_v, data = result, subset = pub == 1, type = "Orwin")
+fsn(yi = d, vi = d_v, data = result, subset = pub == 1, type = "Rosenberg")
+
+
 # Pub bias: 50% of published results are significant ----
 # set simulation parameters
-k = 30 # number of studies
+k = 15 # number of studies
 d.true = 0 # true effect size
 bias = .5 # minimum proportion of published effects that are significant
 
